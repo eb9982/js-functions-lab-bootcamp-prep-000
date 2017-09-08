3dd113294205aa61cee6fdd51b43dfd43f0f51b9
eb9982 <lborsare1@gmail.com>
2017-09-08 02:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // write your code below!
+
 function happyHolidays() {
   return 'Happy holidays!'
 }
-functions happyHolidaysTo(name){
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}`
 }
